03e7ac46696f6af13d3f27af4c5ec7990e173fb6
David Gomes de Carvalho <davidgms31@gmail.com>
2025-03-18 18:29:32 -0300

    add review form
    used v-model to locally save values from the form

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -9,44 +9,46 @@ app.component('product-display', {
   /*html*/
   `<div class="product-display">
     <div class="product-container">
-      <div class="product-image">
-        <img v-bind:src="image">
-      </div>
-      <div class="product-info">
-        <h1>{{ title }}</h1>
-
-        <p v-if="inStock">In Stock</p>
-        <p v-else>Out of Stock</p>
-
-        <p>Shipping: {{ shipping }}</p>
-        <ul>
-          <li v-for="detail in details">{{ detail }}</li>
-        </ul>
-
-        <div 
-          v-for="(variant, index) in variants" 
-          :key="variant.id" 
-          @mouseover="updateVariant(index)" 
-          class="color-circle" 
-          :style="{ backgroundColor: variant.color }">
+        <div class="product-image">
+            <img v-bind:src="image">
         </div>
-        
-        <button 
-          class="button" 
-          :class="{ disabledButton: !inStock }" 
-          :disabled="!inStock" 
-          v-on:click="addToCart">
-          Add to Cart
-        </button>
+        <div class="product-info">
+            <h1>{{ title }}</h1>
 
-      </div>
+            <p v-if="inStock">In Stock</p>
+            <p v-else>Out of Stock</p>
+
+            <p>Shipping: {{ shipping }}</p>
+            <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+            </ul>
+
+            <div 
+            v-for="(variant, index) in variants" 
+            :key="variant.id" 
+            @mouseover="updateVariant(index)" 
+            class="color-circle" 
+            :style="{ backgroundColor: variant.color }">
+            </div>
+            
+            <button 
+            class="button" 
+            :class="{ disabledButton: !inStock }" 
+            :disabled="!inStock" 
+            v-on:click="addToCart">
+            Add to Cart
+            </button>
+        </div>
     </div>
+    <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+    <review-form  @review-submitted="addReview"></review-form>
   </div>`,
   data() {
     return {
         product: 'Socks',
         brand: 'Vue Mastery',
         selectedVariant: 0,
+        reviews: [],
         details: ['50% cotton', '30% wool', '20% polyester'],
         variants: [
           { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
@@ -60,6 +62,9 @@ app.component('product-display', {
       },
       updateVariant(index) {
           this.selectedVariant = index
+      },
+      addReview(review) {
+        this.reviews.push(review)
       }
   },
   computed: {
